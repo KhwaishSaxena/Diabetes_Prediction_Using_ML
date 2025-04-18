430fc74ec33c596a1590c31bdcb3567dec060e7c
Khwaish Saxena <160560474+KhwaishSaxena@users.noreply.github.com>
2025-04-18 18:38:57 +0530

    app.py

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 import streamlit as st
 
 def main():
-    pg = st.navigation([
-        
-        st.title("Diabetes Prediction Using Machine Learning")
-        st.write("Welcome to the Home Page of the Diabetes Prediction App.")
-    ])
-    pg.run()
+    st.title("Diabetes Prediction Using Machine Learning")
+    st.write("Welcome to the Home Page of the Diabetes Prediction App.")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
